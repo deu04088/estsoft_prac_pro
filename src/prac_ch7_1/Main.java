fd53c96430601f4fc0322fa213b3185dce85445f
park <deu04088@naver.com>
2025-08-16 20:14:35 +0900

    Edit: ch7 pro

---
+++ src/prac_ch7_1/Main.java
@@ -0,0 +1,11 @@
+package prac_ch7_1;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog("바둑이");
+        Cat cat = new Cat("나비");
+
+        dog.makeSound();
+        cat.makeSound();
+    }
+}
