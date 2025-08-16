49d501f1910e679f8bdc943498c15744ce5850ff
park <deu04088@naver.com>
2025-08-16 20:15:35 +0900

    Edit: ch7 pro

--- src/prac_ch7_1/Main.java
+++ src/prac_ch7_1/Main.java
@@ -2,8 +2,8 @@ package prac_ch7_1;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("바둑이");
-        Cat cat = new Cat("나비");
+        Animal dog = new Dog("바둑이");
+        Animal cat = new Cat("나비");
 
         dog.makeSound();
         cat.makeSound();
